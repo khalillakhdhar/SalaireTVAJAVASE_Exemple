5291cc397c8b44f621681516788afc0f00ced811
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-05 15:00:09 +0100

    set

--- src/classes/Salaire.java
+++ src/classes/Salaire.java
@@ -3,4 +3,19 @@ package classes;
 public class Salaire {
 	private String nom;
 	private double salaire;
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public double getSalaire() {
+		return salaire;
+	}
+	public void setSalaire(double salaire) {
+		this.salaire = salaire;
+	}
+	
+	
+	
 }
