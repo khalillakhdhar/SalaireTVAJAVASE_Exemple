e34bb0725ce33eb129f868360e652f84715aed7e
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-05 15:13:44 +0100

    Update Test.java

--- src/testes/Test.java
+++ src/testes/Test.java
@@ -3,11 +3,9 @@ package testes;
 import classes.Salaire;
 
 public class Test {
-public static void main(String args[])
-{
-Salaire s=new Salaire("Ali", 2000);
-System.out.println(s.toString());
+	public static void main(String args[]) {
+		Salaire s = new Salaire("Ali", 2000);
+		System.out.println(s.toString());
 
-
-}
+	}
 }
