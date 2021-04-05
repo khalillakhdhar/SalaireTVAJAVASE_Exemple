6493ff43f053cb049f9790055247c716f5c1ffff
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-05 15:12:14 +0100

    main

--- src/testes/Test.java
+++ src/testes/Test.java
@@ -1,9 +1,12 @@
 package testes;
 
+import classes.Salaire;
+
 public class Test {
 public static void main(String args[])
 {
-	
+Salaire s=new Salaire("Ali", 2000);
+System.out.println(s.toString());
 
 
 }
