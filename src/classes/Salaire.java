32c7a03172988ee70d2dce39c4746289f86640bd
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-05 15:09:50 +0100

    Update Salaire.java

--- src/classes/Salaire.java
+++ src/classes/Salaire.java
@@ -3,47 +3,47 @@ package classes;
 public class Salaire {
 	private String nom;
 	private double salaire;
+
 	public String getNom() {
 		return nom;
 	}
+
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+
 	public double getSalaire() {
 		return salaire;
 	}
+
 	public void setSalaire(double salaire) {
 		this.salaire = salaire;
 	}
+
 	public Salaire(String nom, double salaire) {
 		this.nom = nom;
 		this.salaire = salaire;
 	}
-	
-	
-	double calculeTaxes()
-	{
-		if(salaire<500)
+
+	double calculeTaxes() {
+		if (salaire < 500)
 			return 0;
-		else if (salaire <=1500)
-			return salaire *0.1;
-		else 
-			return salaire *0.15;
-		
-		
-		
+		else if (salaire <= 1500)
+			return salaire * 0.1;
+		else
+			return salaire * 0.15;
+
 	}
-	double salaireNette()
-	{
-		
-		return this.salaire-calculeTaxes();
+
+	double salaireNette() {
+
+		return this.salaire - calculeTaxes();
 	}
+
 	@Override
 	public String toString() {
 		return "Détailles de la salaire [nom=" + nom + ", salaire=" + salaire + ", Taxes=" + calculeTaxes()
 				+ ", salaire nette=" + salaireNette() + "]";
 	}
-
-
 
 }
